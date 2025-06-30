158e046f03c741ce6dcfcf99cc91b4439d5250ec
vrayiRAX <ia.badilla@duocuc.cl>
2025-06-29 21:32:42 -0400

    commit

---
+++ Perfulandia-Backend/reseña-perfulandia/src/main/java/com/perfulandia/resena_perfulandia/ResenaPerfulandiaApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.resena_perfulandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenaPerfulandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenaPerfulandiaApplication.class, args);
+	}
+
+}
---
+++ Perfulandia-Backend/reseña-perfulandia/src/test/java/com/perfulandia/resena_perfulandia/ResenaPerfulandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.resena_perfulandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenaPerfulandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
